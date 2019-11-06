7baed02c7a21aa7a5083418fff8d23ca053e5cde
fairb205 <michaelfairbanks5469@gmail.com>
2019-11-06 00:09:50 -0600

    Change Status HTMl

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -3,7 +3,7 @@ import {ModuleWithProviders} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
 import {HomeComponent} from './home/home.component';
 import {UserListComponent} from './users/user-list.component';
-import {TodoListComponent } from "./todos/todo-list.component";
+import {TodoListComponent } from './todos/todo-list.component';
 
 // Route Configuration
 export const routes: Routes = [
